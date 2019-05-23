0aff086ba91dac753742de9a1524cf3af16291d5
Arij <arijsakhraoui18@gmail.com>
2019-05-24 00:12:56 +0200

    Notes

--- frontend/src/app/notesQCMApp/notes-qcmdashboard/notes-qcmdashboard.component.ts
+++ frontend/src/app/notesQCMApp/notes-qcmdashboard/notes-qcmdashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/Services/UserService/user.service';
 import { QCMService } from 'src/app/Services/QCMService/qcm.service';
+import { QCM } from 'src/app/Models/QCM';
+import { notEqual } from 'assert';
 
 @Component({
   selector: 'app-notes-qcmdashboard',
@@ -8,17 +10,49 @@ import { QCMService } from 'src/app/Services/QCMService/qcm.service';
   styleUrls: ['./notes-qcmdashboard.component.scss']
 })
 export class NotesQCMDashboardComponent implements OnInit {
-
-  listQCM: any;
+  listMatieres : Array<string> = ["Maths", "Histoire", "Informatique"];
+  listNotesMatieres= new Map<string, Array<number> >();
+  listQCM: Array<QCM>;
   constructor(private userService: UserService, private qcmService: QCMService) { 
     this.qcmService.getQCMByUser(userService.currentUser.mail).subscribe(
-      (listQCMFromAPI) => {
+      (listQCMFromAPI: Array<QCM>) => {
         this.listQCM = listQCMFromAPI;
+        //this.note();
         console.log('listQCMFrom API ', this.listQCM);
       }
     );
   }
 
+  note(){
+    //this.listMatieres.forEach(matiere => {
+      //let listNotes : Array<number> = [];
+      this.listQCM.forEach(qcm => {
+        let counterQuestionGood = 0;
+        let Note=0;
+        qcm.listQuestions.forEach(question => {
+          let counterChoixGood = 0;
+          question.listChoix.forEach(choix => {
+            if (choix.isValid) {
+              counterChoixGood++;
+            }
+            if (counterChoixGood === question.nbReponseValidQuestion &&
+              question.listChoix.length === question.nbReponseValidQuestion) {
+                counterQuestionGood++;
+            }
+          });
+        })
+        Note = (20/qcm.maxPointQCM) * counterQuestionGood;
+        this.listNotesMatieres.get(qcm.matiereQCM).push(Note);
+        //this.listNotesMatieres.set(qcm.matiereQCM, listNotes);
+        //  counter push dans array
+
+      });
+    //});
+    
+
+    
+
+}
   ngOnInit() {
   }
 
